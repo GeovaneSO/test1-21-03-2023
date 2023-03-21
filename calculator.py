c63c7be1e3eb9a65f43c648358f17197b3ff0857
GeovaneSO <oliveira.5geovane@outlook.com>
2023-03-21 09:13:01 -0300

    creating consumption calculator

--- calculator.py
+++ calculator.py
@@ -1,14 +1,12 @@
 def calculator(consumption: list, distributor_tax: float, tax_type: str) -> tuple:
-    """
-    returns a tuple of floats contained anual savings, monthly savings, applied_discount and coverage
-    """
+
     counter = 0
 
     for number in consumption:
-        counter +=  number
-    
-    average = float(counter)/len(consumption)
-  
+        counter += number
+
+    average = float(counter) / len(consumption)
+
     annual_savings = 0
     monthly_savings = 0
     applied_discount = 0
@@ -20,7 +18,7 @@ def calculator(consumption: list, distributor_tax: float, tax_type: str) -> tupl
             applied_discount = 0.18
 
         if tax_type == "Comercial":
-           applied_discount = 0.16
+            applied_discount = 0.16
 
         if tax_type == "Industrial":
             applied_discount = 0.12
@@ -33,13 +31,12 @@ def calculator(consumption: list, distributor_tax: float, tax_type: str) -> tupl
             applied_discount = 0.22
 
         if tax_type == "Comercial":
-           applied_discount = 0.18
+            applied_discount = 0.18
 
         if tax_type == "Industrial":
             applied_discount = 0.15
 
         coverage = 0.95
-
 
     else:
 
@@ -47,17 +44,17 @@ def calculator(consumption: list, distributor_tax: float, tax_type: str) -> tupl
             applied_discount = 0.25
 
         if tax_type == "Comercial":
-           applied_discount = 0.22
+            applied_discount = 0.22
 
         if tax_type == "Industrial":
             applied_discount = 0.18
 
         coverage = 0.99
-   
-    monthly_savings = (average*distributor_tax) * applied_discount * coverage
-   
-    annual_savings = monthly_savings*12
-   
+
+    monthly_savings = (average * distributor_tax) * applied_discount * coverage
+
+    annual_savings = monthly_savings * 12
+
     return (
         round(annual_savings, 2),
         round(monthly_savings, 2),
